9ee427eafb7bd45ba33689d7762c710e30532bc6
a <161460644+nyashk-a@users.noreply.github.com>
2026-04-14 17:23:19 +0300

    add "Message" and simple listening

--- NetDriver/AD/INetworker.cs
+++ NetDriver/AD/INetworker.cs
@@ -11,7 +11,25 @@ namespace NetDriver.AD
         {
             while (!t.IsCancellationRequested)
             {
-                
+                // header read
+                var h = new byte[21];
+                int read = 0;
+                while (read < h.Length)
+                {
+                    read += await sock.ReceiveAsync(h.AsMemory(read, h.Length - read));
+                }
+                read = 0;
+
+                var conf = Message.PartialParse(h);
+
+                var lastFragment = new byte[conf.expectedSize];
+
+                while (read < conf.expectedSize)
+                {
+                    read += await sock.ReceiveAsync(lastFragment.AsMemory(read, lastFragment.Length - read));
+                }
+
+                var msg = new Message(conf, lastFragment);
             }
         }
     }
--- NetDriver/AD/Message.cs
+++ NetDriver/AD/Message.cs
@@ -1,10 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AVcontrol;
 
-namespace NetDriver.NetDriver.AD
+namespace NetDriver.AD
 {
     public class Message
     {
+        // [content lenght (только длинна полезной нагрузки) : 4]
+        // [message type (определяется из енума) : 1]
+        // [suid : 16]
+        // [content : content lenght]
+        // [(опционально, в зависимости от типа) packNumber : 4]
+
+        public enum Types
+        {
+            ConfigurateMessage = 0b00000000,
+            AnswerToMessage = 0b10000000,
+            RequestMessage = 0b01000000,
+            SingleMessage = 0b11000000,
+            PartFromFileMessage = 0b00100000,
+        }
+
+        private readonly byte[] Data;
+
+        public readonly Types msgType;
+        public readonly Guid msgSuid;
+        public readonly int? packNum;
+
+        public Message(
+            byte[] data,
+            Types type,
+            Guid? suid = null,
+            int? packnum = null
+            )
+        {
+            Data = data;
+            msgType = type;
+            msgSuid = suid == null ? Guid.NewGuid() : suid.Value;
+            packNum = packnum;
+
+            if (msgType == Types.PartFromFileMessage && packNum == null) throw new Exception("cant create message with type Types.PartFromFileMessage and without package number");
+        }
+
+        public Message(MessageConfig msgConf, byte[] pack)
+        {
+            Data = new byte[msgConf.DataSize];
+            Buffer.BlockCopy(pack, 0, Data, 0, msgConf.DataSize);
+
+            msgType = msgConf.msgType;
+            msgSuid = msgConf.msgSuid;
+
+            packNum = msgType == Types.PartFromFileMessage ? FromBinary.LittleEndian<int>(pack.AsSpan(msgConf.DataSize, 4)) : null;
+        }
+
+        public Message(byte[] pack)
+        {
+            msgType = (Types)pack[4];
+            msgSuid = new Guid(pack.AsSpan(5, 16));
+
+            int datasize = FromBinary.LittleEndian<int>(pack.AsSpan(0, 4));
+            Data = new byte[datasize];
+            Buffer.BlockCopy(pack, 16 + 1 + 4, Data, 0, datasize);
+
+            packNum = msgType == Types.PartFromFileMessage ? FromBinary.LittleEndian<int>(pack.AsSpan(21 + datasize, 4)) : null;
+        }
+
+        public byte[] pack { get
+            {
+                var pck = new byte[4 + 1 + 16 + Data.Length + (msgType == Types.PartFromFileMessage ? 4 : 0)];
+                Buffer.BlockCopy(ToBinary.LittleEndian(Data.Length), 0, pck, 0, 4);
+                pck[4] = (byte)msgType;
+                Buffer.BlockCopy(msgSuid.ToByteArray(), 0, pck, 4 + 1, 16);
+                Buffer.BlockCopy(Data, 0, pck, 4 + 1 + 16, Data.Length);
+                if (msgType == Types.PartFromFileMessage) Buffer.BlockCopy(ToBinary.LittleEndian(packNum.Value), 0, pck, 4 + 1 + 16 + Data.Length, 4);
+                return pck;
+            }
+        }
+
+        public static MessageConfig PartialParse(byte[] pack)
+        {
+            if (pack.Length != 21) throw new Exception("pack is to short");
+            return new MessageConfig
+            (
+                FromBinary.LittleEndian<int>(pack.AsSpan(0, 4)),
+                (Types)pack[4],
+                new Guid(pack.AsSpan(5, 16))
+            );
+        }
+        public struct MessageConfig (int ds, Types mt, Guid ms)
+        {
+            public readonly int DataSize = ds;
+            public readonly Types msgType = mt;
+            public readonly Guid msgSuid = ms;
+
+            public readonly int expectedSize = ds + (mt == Types.PartFromFileMessage ? 4 : 0);
+        }
     }
 }
